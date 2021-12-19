071225749867611d5f2fdbbe7006c1e58033977d
mohiaden <mohiaden@eoutlook.com>
2021-12-19 15:55:05 +0100

    ok

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,20 +11,16 @@ import { Hero } from '../hero';
 export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
-  hero : Hero = {
-    id: 1,
-    name: 'Windstorm',
-  }
-
+  
   selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
   constructor() { }
-  
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
